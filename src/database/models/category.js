92526ece93993c4b45c274c5fb8e51d3460205fb
vinidipaula <vinidipaula92@gmail.com>
2022-07-14 19:37:00 -0300

    :heavy_check_mark: feat: Criado as models da table category

---
+++ src/database/models/category.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+
+const attributes = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+};
+
+module.exports = (sequelize) => {
+  const model = sequelize.define('Category', attributes, {
+    tableName: 'Users',
+    timestamps: false,
+  });
+  return model;
+}
